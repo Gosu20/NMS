22cb6401243aeca7be184f78476c22a5d0350cfd
Ananya <workwithananya20@gmail.com>
2026-05-26 07:50:57 +0530

    day-1

---
+++ script.js
@@ -0,0 +1,37 @@
+//variables and datatypes
+// var,let,const
+// var a=20
+// console.log(a)
+
+// var a=30
+// console.log(a)
+// by using var keyword we can re declare a variable and also re assign its value 
+
+// let a =40
+// console.log(a)
+
+// a=50
+// console.log(50)
+// by using let we can only re assign a variable and not re declare it 
+
+// const a=70
+// console.log(a)
+
+// 90
+// console.log(a)
+
+// let a=90
+// let a=80
+// console.log(a)
+// primtive data types and non-primitive data types 
+// let a =10
+// console.log(typeof(a))
+// let a="ananya"
+// console.log(typeof(a))
+// let a =false
+// console.log(typeof(a))
+// let a;
+// console.log(typeof(a))
+// let a=null;
+// console.log(typeof(a))
+//primitive data types-string, number,boolean,undefined,null
